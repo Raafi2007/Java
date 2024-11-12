596b5077cd679b1991460e44eddcb1e9c28c81e3
mohamed.moideen <mohamed.moideen@infoedge.com>
2024-11-12 15:49:48 +0530

    Class example code added

---
+++ src/main/java/classExample/StudentExample01.java
@@ -0,0 +1,19 @@
+package classExample;
+
+public class StudentExample01 {
+
+
+    // data member (also instance variable)
+    int id;
+    // data member (also instance variable)
+    String name;
+
+    public static void main(String args[])
+    {
+        // creating an object of
+        // StudentExample
+        StudentExample01 s1 = new StudentExample01();
+        System.out.println(s1.id);
+        System.out.println(s1.name);
+    }
+}
